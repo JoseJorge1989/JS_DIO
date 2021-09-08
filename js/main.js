888f2d95ff84f13f68d7e871308b866afe814beb
JoseJorge1989 <josejorge821@gmail.com>
2021-09-08 18:10:28 -0300

    javascript

---
+++ js/main.js
@@ -0,0 +1,72 @@
+function clica(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado"; //aplicação do DOM 
+
+    /*alert("obrigado por clicar"); */
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por estudar";
+     elemento.innerHTML = "Obrigado por estudar";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passar o mouse aqui";
+    elemento.innerHTML = "Passar o mouse aqui";
+}
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(5, 30));  */ 
+
+/*
+//exibe um alerta com data e hora
+let D = new Date();
+alert(D.getDay()); */
+
+
+/*
+let count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}; */
+
+/*
+let count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+}  */
+
+
+
+/*
+let idade = prompt("Digite sua idade");
+if (idade >= 18 ){
+    alert("maior de idade");
+}
+else{
+    alert("menor de idade");
+}
+*/
+
+
+/*
+let lista = ["maçã", "uva", "goiaba", "abacate"];
+lista.push("abacaxi");//add item a mais na lista
+/* lista.pop();// remove o ultimo item da lista 
+console.log(lista);//exibe lista completa
+
+console.log(lista[2]);//exibe apenas o intem selecionado
+
+console.log(lista.length);//mostra quantidade de itens na lista  */
+
+
+/*
+let nome = "Jorge";
+let idade  = 32;
+alert (nome + " tem " + idade + " e é desenvolvedor web"); */
